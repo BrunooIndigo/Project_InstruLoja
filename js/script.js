ad7bac0576da6f48dcc3ef9c8eb39cb34d2ab87d
Bruno Henrique Gabriel <brunohenriquegabss180@gmail.com>
2025-05-28 20:02:05 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function butao1() {
+  window.open("/pages/Acusticos.html" , "_blank")
+}
+
+function butao2() {
+  window.open("/pages/guitarras.html" , "_blank")
+}
+
+function butao3() {
+  window.open("/pages/baixos.html" , "_blank")
+}
+
+function butao4() {
+  window.open("/pages/perifa.html" , "_blank")
+}
+
+function butao5() {
+  window.open("/pages/amps.html" , "_blank")
+}
+
+ function rolarAteElemento() {
+      const destino = document.getElementById("cardsob");
+      destino.scrollIntoView({ behavior: "smooth" });
+    }
+ function cardClicavel(){
+    window.open("/pages/compra_pad.html")
+ }
+ function cardClicavelguit(){
+    window.open("/pages/compraguit.html")
+ }
+function cardClicavelbass(){
+    window.open("/pages/comprabass.html")
+ }
+ function cardClicavelperifa(){
+    window.open("/pages/compraperifa.html")
+ }
+ function cardClicavelamp(){
+    window.open("/pages/compraamp.html")
+ }
