24aa58126442a4e492d831c3ec8e437763e0c611
Bruno Henrique Gabriel <brunohenriquegabss180@gmail.com>
2025-05-28 20:34:54 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,39 +1,44 @@
 function butao1() {
-  window.open("Project_InstruLoja/pages/Acusticos.html" , "_blank")
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/Acusticos.html", "_blank");
 }
 
 function butao2() {
-  window.open("Project_InstruLoja/pages/guitarras.html" , "_blank")
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/guitarras.html", "_blank");
 }
 
 function butao3() {
-  window.open("Project_InstruLoja/pages/baixos.html" , "_blank")
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/baixos.html", "_blank");
 }
 
 function butao4() {
-  window.open("Project_InstruLoja/pages/perifa.html" , "_blank")
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/perifa.html", "_blank");
 }
 
 function butao5() {
-  window.open("Project_InstruLoja/blob/main/pages/amps.html" , "_blank")
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/amps.html", "_blank");
 }
 
- function rolarAteElemento() {
-      const destino = document.getElementById("cardsob");
-      destino.scrollIntoView({ behavior: "smooth" });
-    }
- function cardClicavel(){
-    window.open("Project_InstruLoja/blob/main/pages/compra_pad.html")
- }
- function cardClicavelguit(){
-    window.open("Project_InstruLoja/blob/main/pages/compraguit.html")
- }
-function cardClicavelbass(){
-    window.open("Project_InstruLoja/blob/main/pages/comprabass.html")
- }
- function cardClicavelperifa(){
-    window.open("Project_InstruLoja/blob/main/pages/compraperifa.html")
- }
- function cardClicavelamp(){
-    window.open("Project_InstruLoja/blob/main/pages/compraamp.html")
- }
+function rolarAteElemento() {
+  const destino = document.getElementById("cardsob");
+  destino.scrollIntoView({ behavior: "smooth" });
+}
+
+function cardClicavel() {
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/compra_pad.html", "_blank");
+}
+
+function cardClicavelguit() {
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/compraguit.html", "_blank");
+}
+
+function cardClicavelbass() {
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/comprabass.html", "_blank");
+}
+
+function cardClicavelperifa() {
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/compraperifa.html", "_blank");
+}
+
+function cardClicavelamp() {
+  window.open("https://brunooindigo.github.io/Project_InstruLoja/pages/compraamp.html", "_blank");
+}
